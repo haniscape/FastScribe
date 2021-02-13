e87a1562eca916236532a3cfdde048aabe31f6d2
Hany Afifi <hanyafifi@gmail.com>
2021-02-13 15:15:30 +0100

    Fixed styling and responsive

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// Copyright year update
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
+});
+
+
+
+
 const API_QUOTE = 'http://api.quotable.io/random';
