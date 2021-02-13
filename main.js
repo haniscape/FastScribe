b601e7c1042a668525f0c76fd2dab570e4aae63d
Hany Afifi <hanyafifi@gmail.com>
2021-02-14 00:25:23 +0100

    fixed responsive for smaller size screens

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-// Copyright year update
+// Copyright Year updating automatically
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
 });
 
 
 
+// source for the text offered to the user to be typed 
+function getQuote() {
+    return fetch('http://api.quotable.io/random')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            return data.content;
+        });
+}
 
-const API_QUOTE = 'http://api.quotable.io/random';
+
+showQuote();
+
+
+//  variables for Stage Display and Stage Input
+const STAGEDISPLAY = document.querySelector('#stage-display');
+const STAGEINPUT = document.querySelector('stage-input')
+
+// render what returned from the API into the Stage Display,
+// and preparing our Stage Input area
+async function showQuote() {
+    const QUOTE = await getQuote();
+    // console.log(QUOTE);
+    STAGEDISPLAY.innerText = QUOTE;
+    STAGEINPUT.value = null;
+}
+
+
+
